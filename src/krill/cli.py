e568dc742cde9ac203c731f2b3bcd86703d15de7
Woonggi Min <kali2005611@gmail.com>
2025-08-02 15:20:28 +0000

    Fix formatting of Bfloat16 representation in CLI statistics output

--- src/krill/cli.py
+++ src/krill/cli.py
@@ -27,11 +27,11 @@ def cli():
     max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
 
     statistics = \
-        f"    /¯¯¯¯{chr(92)}    🦐 Krill {__version__}: A minimal pretraining trainer for LLMs — from scratch.\n"\
-        f"   ( #|{chr(92)}_ü|   {gpu_stats.name}. \033[1;43;30mNum GPUs = {DEVICE_COUNT}.\033[0m Max memory: {max_memory} GB. Platform: {PLATFORM_SYSTEM}.\n"\
-        f"   ( #{chr(92)}  ƒƒ   Torch: {torch.__version__}. {gpu_stats_snippet} Triton: {triton_version}\n"\
-        f"    {chr(92)} #{chr(92)}      Transformers: {transformers_version}. Bfloat16 = {str(SUPPORTS_BFLOAT16).upper()}. FA2 = {HAS_FLASH_ATTENTION}\n"\
-        f'    /|||{chr(92)}     Source code: https://github.com/minpeter/krill\n'
+        f"  /¯¯¯¯{chr(92)}   🦐 Krill {__version__}: A minimal pretraining trainer for LLMs — from scratch.\n"\
+        f" ( #|{chr(92)}_ü|  {gpu_stats.name}. \033[1;43;30mNum GPUs = {DEVICE_COUNT}.\033[0m Max memory: {max_memory} GB. Platform: {PLATFORM_SYSTEM}.\n"\
+        f" ( #{chr(92)}  ƒƒ  Torch: {torch.__version__}. {gpu_stats_snippet} Triton: {triton_version}\n"\
+        f"  {chr(92)} #{chr(92)}     Transformers: {transformers_version}. Bfloat16 = {SUPPORTS_BFLOAT16}. FA2 = {HAS_FLASH_ATTENTION}\n"\
+        f'  /|||{chr(92)}    Source code: https://github.com/minpeter/krill\n'
 
     print(statistics)
 
